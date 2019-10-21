8887cdd326794388c6f485297475954ba39a9a35
Michael Zhang <miketonny@gmail.com>
2019-10-21 13:37:27 +1300

    class colors

--- src/index.js
+++ src/index.js
@@ -11,19 +11,19 @@ import './App.scss';
 const store = new RootStore();
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBxDJP9E6D49x8OQ78SYKyrQiik79J8CqU",
-  authDomain: "pwc-epgp.firebaseapp.com",
-  databaseURL: "https://pwc-epgp.firebaseio.com",
-  projectId: "pwc-epgp",
-  storageBucket: "pwc-epgp.appspot.com",
-  messagingSenderId: "1025716709700",
-  appId: "1:1025716709700:web:7c696e7f4860cbdc354f11",
-  measurementId: "G-ZE8YSLD2T7"
+  apiKey: 'AIzaSyBxDJP9E6D49x8OQ78SYKyrQiik79J8CqU',
+  authDomain: 'pwc-epgp.firebaseapp.com',
+  databaseURL: 'https://pwc-epgp.firebaseio.com',
+  projectId: 'pwc-epgp',
+  storageBucket: 'pwc-epgp.appspot.com',
+  messagingSenderId: '1025716709700',
+  appId: '1:1025716709700:web:7c696e7f4860cbdc354f11',
+  measurementId: 'G-ZE8YSLD2T7'
 };
 
 firebase.initializeApp(firebaseConfig);
 
-const database = firebase.firestore(); //using firestore
+const database = firebase.firestore(); // using firestore
 
 ReactDOM.render(
   <Provider rootStore={store} db={database}>
